080840fd26c3fa7e98c6bc349b7d805f8796a522
Destiny Smith <dest0914@gmail.com>
2024-07-07 20:59:18 -0500

    Add Week 2 Homework

---
+++ Week2.js
@@ -0,0 +1,19 @@
+let pizzaPlace = "Cheezy Chaps Pizza";
+
+let numberOfToppings = 7;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(
+  "Welcome To ${Cheezy Chaps Pizza}, we have ${numberOfToppings} different toppings available to choose from!"
+);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
